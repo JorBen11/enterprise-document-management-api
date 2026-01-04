66cb8a52ede124c89eb3f169509dd28b0987bdcd
JorBen11 <garciatorresjs@hotmail.com>
2026-01-04 02:03:41 -0500

    First Commit

---
+++ src/main/java/com/jgarcia/enterprise/EnterpriseDocumentManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.jgarcia.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseDocumentManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseDocumentManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jgarcia/enterprise/EnterpriseDocumentManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgarcia.enterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseDocumentManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
